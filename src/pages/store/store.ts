2f13907cb4ddd6d92c10fdfd3ab47ea4475d71f5
Lovit <pereira.j.sandro@gmail.com>
2016-12-25 20:08:58 -0300

    Revert "Revert "fix searchbar""
    This reverts commit 47da5b33b7024485007ac06b7c91158d0a69f897.

--- src/pages/store/store.ts
+++ src/pages/store/store.ts
@@ -9,58 +9,42 @@ import { ModalStore } from '../modalstore/modalstore';
 })
 
 export class StorePage {
-  storeArray;
-  searchQuery: string = '';
-  items: String[];
-  i = 0;
+  storeData;
+  storeList = [];
 
   constructor(public navCtrl: NavController, storage: Storage, public modalCtrl: ModalController) {
     storage.get('storeData').then((val) => {
-      //console.log(JSON.parse(val));
-      this.storeArray = JSON.parse(val);
+      this.storeData = JSON.parse(val);
+      this.initializeItems();
     });
   }
 
   logStore(index){
-    console.log(this.storeArray[index]);
-    this.openModal(this.storeArray[index]);
+    this.openModal(this.storeList[index]);
   }
 
   openModal(selectedStore) {
     let modal = this.modalCtrl.create(ModalStore, selectedStore);
     modal.present();
-
   }
 
   initializeItems() {
-    /*let i: number;
-    let sLenght = this.storeArray.lenght;
-    console.log(this.storeArray.lenght);
-    for (i = 0; i  < sLenght; i ++) {
-      this.items[i ] = this.storeArray[i ].name;
-      console.log(i);
-    }
-    console.log(this.items);*/
-
-    for(let store of this.storeArray){
-      console.log(store.name);
-    }
-
+    this.storeList = this.storeData;
   }
 
   onInput(ev: any) {
-   // Reset items back to all of the items
-   this.initializeItems();
-  /*
-   // set val to the value of the searchbar
-   let val = ev.this.store.name;
+    // Reset items back to all of the items
+    this.initializeItems();
 
-   // if the value is an empty string don't filter the items
-   if (val && val.trim() != '') {
-     this.items = this.items.filter((item) => {
-       return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-     })
-   }
-   */
- }
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.storeList = this.storeList.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
 }
