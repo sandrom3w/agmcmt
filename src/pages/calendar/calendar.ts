c13e9950266f81eeb9df926529df9299aa4f6617
Lovit <pereira.j.sandro@gmail.com>
2016-12-25 20:16:17 -0300

    calendar.ts
    Arrumando erro no calendar ts (component)

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -5,24 +5,14 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-calendar',
   templateUrl: 'calendar.html'
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 })
 export class CalendarPage {
 
-<<<<<<< HEAD
-  constructor(public navCtrl: NavController, storege: Storage) {
-    this.storeCache.storage.get('userData').then((val) => {
-    });
-=======
   constructor(public navCtrl: NavController, storage: Storage) {
     storage.get('userData').then((val) => {
       //console.log(JSON.parse(val));
-    })
-
->>>>>>> origin/master
+    });
   }
 
 }
